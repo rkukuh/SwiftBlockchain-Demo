948dd6221f6f0d5fe1c9091493669c3a84c852b2
R. Kukuh <rkukuh@gmail.com>
2023-10-14 17:07:17 +0700

    Initial Commit

---
+++ SwiftBlockchain Demo/SwiftBlockchain_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBlockchain_DemoApp.swift
+//  SwiftBlockchain Demo
+//
+//  Created by R. Kukuh on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBlockchain_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
